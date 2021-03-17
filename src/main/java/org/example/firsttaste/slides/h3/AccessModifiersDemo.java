fc4a630283dcc07247a2b3ca647731dc112c69cf
bramjanssens <bram.janssens@infosupport.com>
2021-03-17 10:57:14 +0100

    AccessModifiersDemo added.

---
+++ src/main/java/org/example/firsttaste/slides/h3/AccessModifiersDemo.java
@@ -0,0 +1,10 @@
+package org.example.firsttaste.slides.h3;
+
+public class AccessModifiersDemo {
+
+    public static void main(String[] args) {
+        Person p = new Person();
+        String n = p.name;
+    }
+
+}
---
+++ src/main/java/org/example/firsttaste/slides/h3/a/Student.java
@@ -0,0 +1,13 @@
+package org.example.firsttaste.slides.h3.a;
+
+import org.example.firsttaste.slides.h3.Person;
+
+public class Student extends Person { // subclass
+
+    private int studentNr;
+
+    public void doeIets() {
+        System.out.println("Hallo ik ben " + this.name);
+    }
+
+}
