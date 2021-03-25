2ef4c85e950e389b5bd83afb2aa9a658c8ac089c
bramjanssens <bram.janssens@infosupport.com>
2021-03-25 14:09:50 +0100

    My First Sealed Class!! :-)

---
+++ blok1/src/main/java/org/example/java16/SealedShape.java
@@ -0,0 +1,10 @@
+package org.example.java16;
+
+// java 16 needed
+public /*sealed*/ class SealedShape {}
+
+final class Circle extends SealedShape {}
+
+final class Rectangle extends SealedShape {}
+
+final class Square extends SealedShape {}
